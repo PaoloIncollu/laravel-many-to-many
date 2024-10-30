166e0f231fcfd6ce9c89e840d4c98ccba7742f11
PaoloIncollu <167555333+PaoloIncollu@users.noreply.github.com>
2024-10-30 12:57:15 +0100

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'content',
+        'creation_date',
+        'published',
+        'type_id'
+
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.guest')
+
+@section('page-title', 'Welcome')
+
+    @section('main-content')
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center text-primary">
+                            Welcome!
+                        </h1>
+                        <br>
+                        La welcome page è una pagina pubblica (NON protetta)
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    @endsection
